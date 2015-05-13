275681376b1e44c59626c7843b62a4dd3bec0f4a
Oliver Bryant <oliver@ufomail.co.uk>
2015-05-13 21:26:54 +0000

    The first commit
    contains the raw files for building TubeyNET (Codenamed TubeyForWin3)

---
+++ TubeyForWin3/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Net;
+using System.Collections.ObjectModel;
+using Newtonsoft.Json;
+using System.Net.Http;
+
+namespace TubeyForWin3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Get();
+        }
+        async private void Get()
+        {
+            var client = new HttpClient();
+            string display = ("Getting Latest Data");
+            DateTime clockStatus = DateTime.Now;
+            string ok = ("Updated:" + clockStatus);
+            Status.IsEnabled = true;
+            Status.Content = display;
+            var response = await client.GetAsync(new Uri("https://api.tfl.gov.uk/line/mode/tube,overground,dlr,tram,national-rail,cable-car,river-bus,river-tour/status"));
+            var content = await response.Content.ReadAsStringAsync();
+            Status.Content = ok;
+            // This is going to be an ObservableCollection now
+            var LineDisplayCollection = JsonConvert.DeserializeObject<IEnumerable<LineDisplay>>(content);
+            Grid1.ItemsSource = LineDisplayCollection;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Get();
+        }
+    }
+}
---
+++ TubeyForWin3/MyData.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace TubeyForWin3
+{
+    [JsonConverter(typeof(LineDisplayConverter))]
+    public class LineDisplay
+    {
+        public string Name { get; set; }
+        public string SeverityDescription { get; set; }
+        public string Reason { get; set; }
+    }
+    public class LineDisplayConverter : JsonConverter
+    {
+        public override bool CanConvert(Type objectType)
+        {
+            return (objectType == typeof(LineDisplay));
+        }
+
+        public override bool CanWrite { get { return false; } }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            JObject jo = JObject.Load(reader);
+
+
+            LineDisplay newItem = new LineDisplay();
+            newItem.Name = (string)jo.SelectToken("name");
+
+
+            newItem.SeverityDescription = (string)jo.SelectToken("lineStatuses[0].statusSeverityDescription");
+
+            newItem.Reason = (string)jo.SelectToken("lineStatuses[0].reason");
+
+            return newItem;
+        }
+
+
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
