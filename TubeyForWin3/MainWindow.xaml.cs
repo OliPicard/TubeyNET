74944e9426e2778a20e9685eb0f999dc5f427a7b
Oliver Bryant <oliver@ufomail.co.uk>
2015-05-17 19:31:09 +0000

    refactor by Suchiman
    Suchiman has provided a re-factor which should clear up some of the
    legacy code. thanks for making this suck less!

--- TubeyForWin3/MainWindow.xaml.cs
+++ TubeyForWin3/MainWindow.xaml.cs
@@ -1,23 +1,11 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Threading;
+using System.Net.Http;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using System.Net;
-using System.Collections.ObjectModel;
-using Newtonsoft.Json;
-using System.Net.Http;
-
+using System.Windows.Threading;
 
 namespace TubeyForWin3
 {
@@ -40,42 +28,44 @@ namespace TubeyForWin3
             InitializeComponent();
             Get();
         }
+
         async private void Get()
         {
-            var client = new HttpClient(); //spin up a new HttpClient
-            string display = "Getting Latest Data"; //just a string..
-            DateTime clockStatus = DateTime.Now; //what's the time?
-            string ok = "Updated:" + clockStatus; //ok string includes timestamp
-            Status.IsEnabled = true; //enable the Sus Label
-            Status.Content = display; //Initialize request
+            var client = new HttpClient();
+
+            Status.IsEnabled = true;
+            Status.Content = "Getting Latest Data";
+
             var response = await client.GetAsync(new Uri("https://api.tfl.gov.uk/line/mode/tube,overground,dlr,tram,national-rail,cable-car,river-bus,river-tour/status"));
             var content = await response.Content.ReadAsStringAsync(); //read json string Async.
-            Status.Content = ok; //Status is green, prepair for conversion.
+
+            Status.Content = "Updated:" + DateTime.Now.ToString(); //Status is green, prepair for conversion.
+
             var LineDisplayCollection = JsonConvert.DeserializeObject<IEnumerable<LineDisplay>>(content); // This is going to be an ObservableCollection now
             Grid1.ItemsSource = LineDisplayCollection; //bind the data to the grid.
             Grid1.IsEnabled = true; //enable the grid for user to see.
         }
+
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             Get();
         }
+
         public bool handle = true;
-        private bool check { get; set; } //setting the bools for timekeeper
-        private bool checkten { get; set; }
-        private bool checktwenty { get; set; }
-        private bool checkthirty { get; set; }        
+
         private void wombo_DropDownClosed(object sender, EventArgs e)
         {
             if (handle) Handle();
             handle = true;
         }
+
         private void wombo_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             ComboBox cmb = sender as ComboBox;
             handle = !cmb.IsDropDownOpen; //if the object has been opened.
             Handle(); //switch logic. what am i grabbing?
-
         }
+
         private void Handle()
         {
             string selection = wombo.SelectedItem.ToString().Split(new string[] { ": " }, StringSplitOptions.None).Last();
@@ -84,11 +74,14 @@ namespace TubeyForWin3
                 ConfigureTimer(0);
                 return;
             }
-            int intervall = Int32.Parse(selection.Remove(2).TrimEnd()); //grabs selection from string, trim end removes whitespace and select remove 2 removes the space the the minutes.
+
+            int intervall = Int32.Parse(selection.Remove(2).TrimEnd());
             ConfigureTimer(intervall);
             Get();
         }
+
         private DispatcherTimer UpdateTimer;
+
         private void ConfigureTimer(int minuteIntervall)
         {
             //initialize the timer if it didn't happen before
@@ -113,6 +106,6 @@ namespace TubeyForWin3
             //configure new intervall and restart timer
             UpdateTimer.Interval = TimeSpan.FromMinutes(minuteIntervall);
             UpdateTimer.Start();
-        }   
+        }
     }
 }
